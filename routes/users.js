650a0c88d042bc9893f1ebe39fee5dbc3c845eb7
KuangPF <1633397286@qq.com>
2020-05-24 09:40:28 +0800

    feat: login page

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+// Login Page
+router.get('/login', (req, res) => res.render('login'))
 // Register Page
-router.get('/register', (req, res) => res.render('register'));
+router.get('/register', (req, res) => res.render('register'))
 
-module.exports = router;
+module.exports = router
