75306468388d3f294036d6a382690dc3b3e922ed
KuangPF <1633397286@qq.com>
2020-05-25 23:34:23 +0800

    feat(register): /register api

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,32 @@ router.get('/login', (req, res) => res.render('login'))
 // Register Page
 router.get('/register', (req, res) => res.render('register'))
 
+// Register
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body
+  let errors = []
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' })
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' })
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' })
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  }
+})
+
 module.exports = router
