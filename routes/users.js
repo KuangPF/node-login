748d85ded351ac32338e380e346e9812309b24a9
KuangPF <1633397286@qq.com>
2020-06-03 23:05:50 +0800

    feat: handle register logic

--- routes/users.js
+++ routes/users.js
@@ -31,6 +31,39 @@ router.post('/register', (req, res) => {
       password,
       password2
     })
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' })
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        })
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+            newUser
+              .save()
+              .then(user => {
+                req.flash('success_msg', 'You are now registered and can log in')
+                res.redirect('/users/login')
+              })
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
   }
 })
 
